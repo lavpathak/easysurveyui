57f04553ee228f60d6871af7cb9aca97f3c62649
lavpathak <lav.pathak@gmail.com>
2018-01-24 17:04:41 -0700

    Get all active surveys.

--- src/containers/ActiveSurveys.js
+++ src/containers/ActiveSurveys.js
@@ -2,13 +2,33 @@ import React, {Component} from 'react';
 import {Grid, Container, Header} from 'semantic-ui-react';
 
 export default class ActiveSurvey extends Component {
+
+    constructor() {
+        super();
+        this.state = {
+            surveys: []
+        }
+    }
+
+    componentDidMount() {
+        fetch('http://localhost:8081/surveys')
+        .then(response => response.json())
+        .then(data => this.setState({surveys: data.results}));
+    }
+
     render() {
+        const { surveys } = this.state;
+
         return (
             <Grid padded>
                 <Grid.Row centered>
                     <Container>
                         <Header as='h1'>Active Surveys</Header>
-
+                        <div>
+                            {surveys.map(survey =>
+                              <a href={survey.id}>{survey.name}</a>
+                          )}
+                        </div>
                     </Container>
                 </Grid.Row>
             </Grid>
--- src/containers/MainLayout.js
+++ src/containers/MainLayout.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import MainHeader from './MainHeader';
+import MainHeader from '../components/MainHeader';
 import ActiveSurveys from './ActiveSurveys';
 import {Grid} from 'semantic-ui-react';
 import '../styles.less';
